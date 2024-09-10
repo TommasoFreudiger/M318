8374c8ad1cbf754cb81bfd62680eea69dc556295
Tommaso Freudiger <tommaso.freudiger@gmail.com>
2024-09-10 14:12:24 +0200

    udpate

---
+++ Compiti/03Ridimensiona/03Ridimensiona.2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _03Ridimensiona._2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Compiti/03Ridimensiona/03Ridimensiona.2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+
+namespace _03Ridimensiona._2
+{
+    public partial class MainPage : ContentPage
+    {
+        int ret_base = 150;
+        int ret_larghezza = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ret_larghezza = Convert.ToInt32(entry_dim.Text);
+
+            if (ret_larghezza <= ret_base || ret_larghezza > Window.Width)
+            {
+                rettangolo.WidthRequest = ret_base;
+            }
+            else 
+            {
+                rettangolo.WidthRequest = ret_larghezza;
+            }
+        }
+
+    }
+}
